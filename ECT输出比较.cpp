b6fc153d50fcff6047fb08f106ba58b11066d65f
xialiang1 <772861292@qq.com>
2021-11-11 20:25:03 +0800

    完善
    进一步完善，按键，模数递减，输入捕捉，数码管动态扫描等问题并未解决

--- ECT输出比较.cpp
+++ ECT输出比较.cpp
@@ -1,4 +1,3 @@
-
 #include <hidef.h>      /* common defines and macros */
 #include "derivative.h"      /* derivative-specific definitions */
 #define LED_dir DDRB
@@ -24,7 +23,9 @@
 #define BUS_CLOCK 32000000 
 byte shuma[20]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,       
                 0xbf,0x86,0xdb,0xcf,0xe6,0xed,0xfd,0x87,0xff,0xef};
-  void INIT_PLL(void)  //ÂàùÂßãÂåñÈîÅÁõ∏ÁéØ
+                
+                
+void INIT_PLL(void)  //≥ı ºªØÀ¯œ‡ª∑
 {
     CLKSEL &= 0x7f;       
     PLLCTL &= 0x8F;       
@@ -45,7 +46,9 @@ byte shuma[20]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,
     while(!(CRGFLG&0x08)); 
     CLKSEL |= 0x80;        
 }
-void INIT_port(void)//ÂàùÂßãÂåñÊï∞Á†ÅÁÆ° 
+
+
+void INIT_port(void)//≥ı ºªØ ˝¬Îπ‹ 
 {
   CONT1_dir = 1;
   CONT2_dir = 1;
@@ -56,10 +59,24 @@ void INIT_port(void)//ÂàùÂßãÂåñÊï∞Á†ÅÁÆ°
   CONT3 = 0;
   CONT4 = 0;
   DATA_dir = 0xff;
-  DATA = 0x00;//‰ΩçÈÄâ‰ΩéÁîµÈ¢ë
+  DATA = 0x00;
 }
 
-void initialize_ect(){  //ÂàùÂßãÂåñECTÊ®°Âùó
+
+void init_key(void)  //≥ı ºªØ∞¥º¸
+{
+     KEY1_dir =0;       
+     KEY2_dir=0;
+     KEY3_dir=0;
+     KEY4_dir=0;
+     PPSH = 0x00;		      
+     PIFH = 0x0f;					
+     PIEH = 0x0f;		      
+}
+
+
+void Output_Compare()   //≥ı ºªØ ‰≥ˆ±»Ωœ
+{                      
   ECT_TSCR1_TFFCA=1;
   ECT_TSCR1_TEN=1;
   ECT_TIOS=0xff;
@@ -70,16 +87,153 @@ void initialize_ect(){  //ÂàùÂßãÂåñECTÊ®°Âùó
   ECT_TFLG1=0xff;
   ECT_TFLG2=0xff;
 }
-void clock()//ËÆæÁΩÆECTÊó∂Èó¥‰∏∫1s;
+
+
+void Input_Capture()   //≥ı ºªØ ‰»Î≤∂◊Ω
+{  
+  ECT_TSCR1_TFFCA=1;
+  ECT_TSCR1_TEN=1;
+  ECT_TIOS=0x00;
+  ECT_TCTL1=0x00;
+  ECT_TCTL2=0x00;
+  ECT_TIE=0x00;
+  ECT_TSCR2=0xc7;
+  ECT_TFLG1=0xff;
+  ECT_TFLG2=0xff;
+}
+
+
+
+void INIT_MDC()     //≥ı ºªØƒ£ ˝µ›ºı
 {
+  ECT_MCCTL=0xc7;
+  ECT_MCCNT=50000;
+  ECT_MCCTL_FLMC=1;
+}
+  
+
+
+#pragma CODE_SEG __NEAR_SEG NON_BANKED
+interrupt void PTH_inter(void) 
+{
+   if(PIFH != 0) //≈–∂œ÷–∂œ±Í÷æ    
+   {
+      PIFH = 0xff;//«Â≥˝÷–∂œ±Í÷æ     
+      if(KEY1 == 0)         
+      {
+         time_jia();
+      }
+      if(KEY2 == 0) 
+      {
+          time+=1;
+          if(time>10)
+              time=10;
+      }
+      if(KEY3 == 0)
+          direction=0;
+      if(KEY4 == 0)
+          direction=1;
+   }
+}
+#pragma CODE_SEG DEFAULT
+
+
+
+
+void clock1()//…Ë÷√ECTµ›º” ±º‰Œ™1s;
+{
+  Output_Compare();
   unsigned int i,j;
   for(i=0;i<32;i++){
     ECT_TC0=ECT_TCNT+31250;
-  }
-  void delay(){ //Âª∂Êó∂ÂáΩÊï∞
+  } 
+}
+
+
+
+void clock2()  // …Ë÷√ECTµ›ºı ±º‰Œ™1s;
+{
+  Input_Capture()
+  unsigned int i,j;
+  for(i=0;i<40;i++){
+     INIT_MDC();
+  } 
+}
+
+
+
+
+void delay()   //—” ±∫Ø ˝
+ { 
     unsigned int i;
     for(i=0;i<5000;i++);
+ }
+ 
+ 
+void  time_jia()
+{
+  unsigned int i=0,j=0,k=0;
+  clock1();
+  if(ECT_TFLG1_C0F==1){
+  i=i+1;
+  CONT4=1;
+  DATA=shuma[i];
+  delay();
+  if(i>9){
+  j=j+1;
+  i=0;
+  CONT4=0;
+  CONT3=1;
+  DATA= shuma[j];
+  delay();
   }
+  if(j>6){
+    j=0;
+    if(k==2)break;//  ˝¬Îπ‹‘⁄2∑÷÷” ±Õ£÷π
+    k=k+1;
+    CONT4=0;
+    CONT3=0;
+    CONT2=1;
+    DATA=shuma[k];
+    delay();
+  }
+  if(k==1){
+    LED=0x7f;
+  }
+  if(k==2){
+    LED=0x3f;
+  } 
+ }
+ 
+ 
+ 
+  
+void time_jian()
+{
+  unsigned int i=10,j=6,k=2;
+  clock2();
+  if(ECT_TFLG1_C0F==1){
+    i=i-1;
+    j=j-1;
+    k=k-1;
+    CONT2=1;
+    CONT3=0;
+    CONT4=0;
+    DATA=shuma[k];
+    delay();
+    CONT2=0;
+    CONT3=1;
+    CONT4=0;
+    DATA=shuma[j];
+    delay();
+    CONT2=0;
+    CONT3=0;
+    CONT4=1;
+    DATA=shuma[i];
+ 
+    
+  
+
   
 void main()
 {
@@ -89,17 +243,11 @@ DisableInterrupts;
   C0NT4=1;
   DATA=shuma[0];
  EnableInterrupts;
- unsigned int i=0,j=0,k=0;
  for( ; ; ){
-  initialize_ect();
- if(ECT_TFLG1_C0F==1){
-  i=i+1;
-  CONT4=1;
-  DATA=shuma[i];
-  if(i>9){
-    
+  
  }
   
+    
     
   
  
